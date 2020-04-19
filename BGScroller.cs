9b4829eb239c3f7f6b67ca33de8708b23cb39c31
cldukeshire <60021012+cldukeshire@users.noreply.github.com>
2020-04-19 18:06:44 -0400

    Add files via upload

---
+++ BGScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+	public GameController gameController;
+	private Vector3 startPosition;
+
+	void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+
+		if (gameController.score >= 200)
+		{
+			scrollSpeed = -50;
+        }
+	}
+}
---
+++ starfield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starfield : MonoBehaviour
+{
+    public GameController gameController;
+    private ParticleSystem ps;
+    public float hSliderValue = 1.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.score >= 200)
+        {
+            var main = ps.main;
+            main.simulationSpeed = hSliderValue;
+        }
+    }
+}
